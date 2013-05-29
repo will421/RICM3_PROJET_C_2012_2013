e1ef0438f69e705c71ebbd5064cfab953637f3b0
will421 <will.421@hotmail.fr>
2013-05-29 12:07:15 +0200

    init

---
+++ compression_lzw/test.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   test.h
+ * Author: Will
+ *
+ * Created on 29 mai 2013, 12:07
+ */
+
+#ifndef TEST_H
+#define	TEST_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* TEST_H */
+
